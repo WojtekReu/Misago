c37efc0dc66e717f47d78e21538f89d9d6036192
rafalp <kontakt@rpiton.com>
2020-12-26 23:49:52 +0100

    Add tests for markdown support

--- misago/richtext/plugins/__init__.py
+++ misago/richtext/plugins/__init__.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from mistune import PLUGINS
 
 from .linebreaker import linebreaker
@@ -7,3 +8,7 @@ PLUGINS.update({"simple_linebreak": linebreaker, "bbcode": bbcode})
 default_plugins = [linebreaker, bbcode]
 
 __all__ = ["default_plugins", "linebreaker", "bbcode"]
+=======
+from .hardbreak import plugin_hard_break
+from .shortimage import plugin_short_image
+>>>>>>> Add tests for markdown support
